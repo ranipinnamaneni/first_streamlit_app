c4c6ae47c24020a60b14ba3b51e480e3b563f9a1
ranipinnamaneni <133064438+ranipinnamaneni@users.noreply.github.com>
2023-05-09 18:31:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard boiled free-range egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 and Blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard boiled free-range egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
