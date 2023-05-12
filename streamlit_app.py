359f57a423aa773944a1efc82fc618d2c9d3630c
ranipinnamaneni <133064438+ranipinnamaneni@users.noreply.github.com>
2023-05-12 01:33:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,12 +41,11 @@ try:
   else:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
-  
-#####
+      
 
-streamlit.stop()
 
 #import snowflake.connector
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
